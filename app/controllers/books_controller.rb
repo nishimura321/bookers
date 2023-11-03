21072f20bf3278a2196c587328b7e3d41f5f5376
nishimura321 <146441655+nishimura321@users.noreply.github.com>
2023-11-03 14:41:03 +0000

    [Finish]フラッシュメッセージの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,17 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
